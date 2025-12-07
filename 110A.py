fb1b83b332aebdafa9bf8a4dbbd62aac2ad0edf2
Vedantsd <vedantdesai767@gmail.com>
2025-12-07 16:48:36 +0530

    Solution to 110A

---
+++ 110A.py
@@ -0,0 +1,9 @@
+num = int(input())
+count = 0
+
+while num > 0: 
+    digit = num % 10
+    if digit == 4 or digit == 7: count += 1
+    num = num // 10
+
+print("YES" if (count == 4 or count == 7) else "NO")
