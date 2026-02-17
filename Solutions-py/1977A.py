3fe7213219aeda81c998cb139110c614aee5a150
Vedantsd <vedantdesai767@gmail.com>
2026-02-17 22:33:09 +0530

    Solution to 1977A

---
+++ Solutions-py/1977A.py
@@ -0,0 +1,8 @@
+t = int(input())
+for _ in range(t): 
+    n, m = map(int, input().split())
+    
+    if (n == m or (n > m and (n - m) % 2 == 0)) : 
+        print("YES")
+    else :
+        print("NO")
