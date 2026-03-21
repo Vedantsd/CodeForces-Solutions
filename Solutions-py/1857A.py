511afd0aba82afdb4b2d8c3e8f8d3f853022b322
Vedantsd <vedantdesai767@gmail.com>
2026-03-21 23:18:55 +0530

    Solution to 1857A

---
+++ Solutions-py/1857A.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    odd_count = sum(1 for x in arr if x % 2)
+    
+    if odd_count % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
